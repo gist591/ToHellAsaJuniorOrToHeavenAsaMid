b7bddb71758c6d3f23941ceda7cde85c4366c29f
Leonid Laktionov <lglaktionov@edu.hse.ru>
2025-07-25 02:54:56 +0300

    Небольшые стилевые правки

---
+++ tests/domain/services/conftest.py
@@ -0,0 +1,19 @@
+import pytest
+from uuid import UUID
+from sqlalchemy.ext.asyncio import AsyncSession  # Исправлено!
+
+from to_the_hell.oncallhub.infra.db import get_session
+from to_the_hell.oncallhub.infra.db.models.user import UserORM  # Добавьте импорт
+
+
+class FakeUserRepostitory():
+    def __init__(self, session: AsyncSession) -> None:
+        self.session = session
+
+    async def create(self, user_id: UUID, name: str, telegram_username: str) -> UserORM:
+        user = UserORM(user_id, name, telegram_username)
+
+        self.session.add(user)
+        await self.session.commit()
+        await self.session.refresh(user)
+        return user
